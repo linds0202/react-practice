28106eff2113644e21dda1f62ef72c2005f07c0f
linds0202 <lindsayarainey@gmail.com>
2023-01-06 14:13:30 -0800

    handle click events within  components

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -7,13 +7,29 @@ const Content = () => {
         const int = Math.floor(Math.random() * names.length)
     
         return names[int]
-      }
+    }
+
+    const handleClick = () => {
+        console.log('You clicked it!')
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked!`)
+    }
+
+    const handleClick3 = (e) => {
+        console.log(e.target)
+    }
+    
 
     return (
     <main>
-        <p>
+        <p onDoubleClick={handleClick}>
           Welcome, {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click It!</button>
+        <button onClick={() => handleClick2('Lindsay')}>Click It!</button>
+        <button onClick={(e) => handleClick3(e)}>Click It!</button>
     </main>
   )
 }
