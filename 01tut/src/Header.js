c95b817ce0acedec327082c06796a139f480c5ec
linds0202 <lindsayarainey@gmail.com>
2023-01-06 14:05:03 -0800

    style components index.css

--- 01tut/src/Header.js
+++ 01tut/src/Header.js
@@ -1,6 +1,12 @@
 
 const Header = () => {
-  return (
+  
+    // const styles = {
+    //     backgroundColor: "mediumblue",
+    //     color: "#fff"
+    // }
+  
+    return (
     <header>
         <h1>Grocery List</h1>
     </header>
