966e07540e523bc1a55cdd136af120efcbfed2fd
linds0202 <lindsayarainey@gmail.com>
2023-01-12 09:02:01 -0800

    adjust css widths

--- 02tut/src/App.js
+++ 02tut/src/App.js
@@ -68,34 +68,33 @@ function App() {
   }
 
   return (
-    <div className="App">
-        <Routes>
-          <Route path="/" element={<Layout 
-            search={search}
-            setSearch={setSearch}
-          />}>
-            <Route index element={<Home 
-              posts={searchResults}
-            />} />
-            <Route path='post'>
-              <Route index element={<NewPost 
-                handleSubmit={handleSubmit}
-                postTitle={postTitle}
-                setPostTitle={setPostTitle}
-                postBody={postBody}
-                setPostBody={setPostBody}
-              />} />
-              <Route path=':id' element={<PostPage 
-                posts={posts} 
-                handleDelete={handleDelete} />} />
-            </Route>
-            <Route path='about' element={<About />} />
-            <Route path='*' element={<Missing />} />
-            
-          </Route>
+    <Routes >
+      <Route path="/" element={<Layout 
+        search={search}
+        setSearch={setSearch}
+      />}>
+        <Route index element={<Home 
+          posts={searchResults}
+        />} />
+        <Route path='post'>
+          <Route index element={<NewPost 
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />} />
+          <Route path=':id' element={<PostPage 
+            posts={posts} 
+            handleDelete={handleDelete} />} />
+        </Route>
+        <Route path='about' element={<About />} />
+        <Route path='*' element={<Missing />} />
+        
+      </Route>
 
-        </Routes>
-    </div>
+    </Routes>
+
   );
 }
 
