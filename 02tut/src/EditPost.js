e7c6db7eaa1a7b00a9012a0f094f94ea8e6c04e2
linds0202 <lindsayarainey@gmail.com>
2023-01-16 09:57:19 -0800

    refactor state to use easy peasy instead of context

--- 02tut/src/EditPost.js
+++ 02tut/src/EditPost.js
@@ -1,37 +1,33 @@
-
 import { useParams, Link, useNavigate } from "react-router-dom"
-import { useState, useEffect, useContext } from 'react'
+import { useEffect } from 'react'
 import { format } from 'date-fns'
-import api from './api/posts'
-import DataContext from './context/DataContext'
+import { useStoreState, useStoreActions } from 'easy-peasy'
 
 const EditPost = () => {
-    const [editTitle, setEditTitle] = useState('')
-    const [editBody, setEditBody] = useState('')
 
-    const { posts, setPosts } = useContext(DataContext)
     const { id } = useParams()
     const navigate = useNavigate()
+
+    const editTitle = useStoreState(state => state.editTitle)
+    const editBody = useStoreState(state => state.editBody)
+
+    const editPost = useStoreActions(actions => actions.editPost)
+    const setEditTitle = useStoreActions(actions => actions.setEditTitle)
+    const setEditBody = useStoreActions(actions => actions.setEditBody)    
      
-    const post = posts.find(post => (post.id).toString() === id)
+    const getPostById = useStoreState(state => state.getPostById)
+    const post = getPostById(id)
     
     useEffect(() => {
         setEditTitle(post.title)
         setEditBody(post.body)
     }, [post, setEditTitle, setEditBody])
 
-    const handleEdit = async (id) => {
+    const handleEdit = (id) => {
         const dateTime = format(new Date(), 'MMMM dd, yyyy pp')
         const updatedPost = {id, title: editTitle, dateTime, body: editBody}
-        try {
-          const response = await api.put(`/posts/${id}`, updatedPost)
-          setPosts(posts.map(post => post.id === id ? { ...response.data } : post))
-          setEditTitle('')
-          setEditBody('')
-          navigate('/')
-         } catch (err) {
-          console.log(`Error: ${err.message}`)
-        }
+        editPost(updatedPost)
+        navigate(`/post/${id}`)
     }
 
   return (
@@ -55,7 +51,7 @@ const EditPost = () => {
                     value={editBody}
                     onChange={(e) => setEditBody(e.target.value)}
                 />
-                <button type='submit' onClick={() => handleEdit(post.id)}>Submit</button>
+                <button type='button' onClick={() => handleEdit(post.id)}>Submit</button>
                 </form>
             </>
         }
