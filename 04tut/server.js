e2201a6b674556eabf7887caf5e7e88155e21063
linds0202 <lindsayarainey@gmail.com>
2023-01-18 11:39:02 -0800

    add built in and custom middleware

--- 04tut/server.js
+++ 04tut/server.js
@@ -1,7 +1,37 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
+const { logger } = require('./middleware/logEvents')
+const errorHandler = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
+
+//custom middleware logger
+app.use(logger)
+
+//Cross origin resource sharing
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const corsOptions = { 
+    origin: (origin, callback) => {
+        if(whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+
+//built in middleware to handle url encoded data - form data
+///content-type: application/x-www--form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+//built in middleware for json
+app.use(express.json())
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')))
 
 //  '^/$|/index.html' must begin with / and end with / OR index.html or without the .html
 app.get('^/$|/index(.html)?', (req, res) => {
@@ -44,8 +74,18 @@ const three = (req, res) => {
 app.get('/chain(.html)?', [one, two, three])
 
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ error: '404 not found' })
+    } else {
+        res.type('txt').send('404 not found')
+    }
 })
+
+//
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
