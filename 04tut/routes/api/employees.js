776ee912bf72ce84b1bd71853c4c55be869638fe
linds0202 <lindsayarainey@gmail.com>
2023-01-19 14:25:31 -0800

    add refresh token, logout controller, and credentials

--- 04tut/routes/api/employees.js
+++ 04tut/routes/api/employees.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const router = express.Router()
-const employeesController = require('../../controllers/employeesController')
+const express = require('express');
+const router = express.Router();
+const employeesController = require('../../controllers/employeesController');
 
 router.route('/')
     .get(employeesController.getAllEmployees)
     .post(employeesController.createNewEmployee)
     .put(employeesController.updateEmployee)
-    .delete(employeesController.deleteEmployee)
+    .delete(employeesController.deleteEmployee);
 
 router.route('/:id')
-    .get(employeesController.getEmployee)
+    .get(employeesController.getEmployee);
 
-module.exports = router
+module.exports = router;
