6eb2810148bc897b81f80747f93a0f001e23b010
linds0202 <lindsayarainey@gmail.com>
2023-01-19 12:09:01 -0800

    add auth route

--- 04tut/routes/register.js
+++ 04tut/routes/register.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const registerController = require('../controllers/registerController')
-const { route } = require('./root')
 
 router.post('/', registerController.handleNewUser)
 
