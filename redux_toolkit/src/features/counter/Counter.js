51f982645dfdce96e79ad4a0e51c2c45d223b199
linds0202 <lindsayarainey@gmail.com>
2023-01-26 10:12:05 -0800

    add increment by amount and reset functions

--- redux_toolkit/src/features/counter/Counter.js
+++ redux_toolkit/src/features/counter/Counter.js
@@ -1,10 +1,27 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './counterSlice'
+import { 
+    increment,
+    decrement,
+    reset,
+    incrementByAmount
+} from './counterSlice'
+import { useState } from 'react'
+
 
 const Counter = () => {
 
     const count = useSelector((state) => state.counter.count)
     const dispatch = useDispatch()
+
+    const [ incrementAmount, setIncrementAmount ] = useState(0)
+
+    //if it's NAN then set amount to 0 
+    const addValue = Number(incrementAmount) || 0
+
+    const resetAll = () => {
+        setIncrementAmount(0)
+        dispatch(reset())
+    }
 
   return (
     <section> 
@@ -12,6 +29,16 @@ const Counter = () => {
         <div>
             <button onClick={ () => dispatch(decrement()) }>-</button>
             <button onClick={ () => dispatch(increment()) }>+</button>
+        </div>
+        <input 
+            type='text'
+            value={incrementAmount}
+            onChange={ (e) => setIncrementAmount(e.target.value) }
+        />
+
+        <div>
+            <button onClick={ () => dispatch(incrementByAmount(addValue)) }>Add amount</button>
+            <button onClick={ () => dispatch(resetAll()) }>Reset all</button>
         </div>
     </section>
   )
