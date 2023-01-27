278814be15965ca89cb20e42ab87e9918f94a00e
linds0202 <lindsayarainey@gmail.com>
2023-01-27 12:29:07 -0800

    refactor App.js and PostsList to load posts immediately

--- bulletin_board/src/features/posts/postsSlice.js
+++ bulletin_board/src/features/posts/postsSlice.js
@@ -131,7 +131,7 @@ const postsSlice = createSlice({
                 action.payload.date = new Date().toISOString();
                 action.payload.reactions = {
                     thumbsUp: 0,
-                     wow: 0,
+                    wow: 0,
                     heart: 0,
                     rocket: 0,
                     coffee: 0
