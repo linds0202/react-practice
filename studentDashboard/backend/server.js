ab262dd3cf007a1a3362f846eaf1fec9cf1a5ea0
linds0202 <lindsayarainey@gmail.com>
2023-02-02 11:22:17 -0800

    add auth routes

--- studentDashboard/backend/server.js
+++ studentDashboard/backend/server.js
@@ -27,6 +27,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/root'))
+app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/notes', require('./routes/notesRoutes'))
 app.use('/resources', require('./routes/resourcesRoutes'))
