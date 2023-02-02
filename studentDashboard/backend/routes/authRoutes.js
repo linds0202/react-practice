38dc5664e9a51b4dc023a1abb6c256bac03bb30e
linds0202 <lindsayarainey@gmail.com>
2023-02-02 11:51:08 -0800

    add jwt tokens

--- studentDashboard/backend/routes/authRoutes.js
+++ studentDashboard/backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
-const { loginLimiter } = require('../middleware/loginLimiter')
+const loginLimiter = require('../middleware/loginLimiter')
+
 router.route('/')
     .post(loginLimiter, authController.login)
 
