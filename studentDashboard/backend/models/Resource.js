7ba1f80f043311919d8a1f86025063904c51fcc4
linds0202 <lindsayarainey@gmail.com>
2023-02-06 09:25:40 -0800

    debug resource model

--- studentDashboard/backend/models/Resource.js
+++ studentDashboard/backend/models/Resource.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose')
 
-const resourceSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        desc: {
-            type: String,
-            required: true
-        },
-        link: {
-            type: String,
-            required: true
-        },
-        tags: {
-            type: [String],
-            default: ['Online]'
-        },
-    }
-)
+const resourceSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: [String],
+        default: ['Online']
+    },
+})
 
 module.exports = mongoose.model('Resource', resourceSchema)
